c364f137d2f53d5f4f7b8f1dd04272602274ffe2
Scott <saf6260@rit.edu>
2018-06-30 20:52:15 -0600

    Changed the way the script is working to emulate a facebook kind of reaction

--- web_development/javascript/script.js
+++ web_development/javascript/script.js
@@ -1,2 +1,51 @@
-var list = ["tiger", "cat", "bear", "bird"];
-console.log(list[1]);
+var database = [
+    {
+        username: "scott",
+        password: "supersecret"
+    },
+    {
+        username: "Sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning"
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript is cool!"
+    },
+    {
+        username: "Mitch",
+        timeline: "Java script is sooooooo sweet"
+    }
+];
+
+var userNamePrompt = prompt("Whats your username?");
+var passwordPrompt = prompt("Whats your password?");
+
+function isuservalid(user, pass){
+    for (var i = 0; i < database.length; i++){
+        if (user === database[i].username && pass === database[i].password){
+            return true;
+        }
+    }
+    return false;
+}
+
+function signIn(user, pass) {
+    if (isuservalid(user, pass)){
+        console.log(newsFeed);
+    }else{
+        alert("Sorry, wrong username and password");
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);

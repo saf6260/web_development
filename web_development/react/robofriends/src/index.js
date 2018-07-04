ff1b5f33f60609542280d7025fdb49316def0314
Scott <saf6260@rit.edu>
2018-07-03 23:41:16 -0600

    Started working on react part of course

---
+++ web_development/react/robofriends/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardList from './CardList';
+import './index.css';
+import 'tachyons'
+import {robots} from './robots';
+
+ReactDOM.render(
+    <div>
+        <CardList robots={robots}/>
+    </div>,
+  document.getElementById('root')
+);

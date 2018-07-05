6a5675ac03978089285cdb01c07ecefe44f48a78
Scott <saf6260@rit.edu>
2018-07-05 12:45:02 -0600

    Cleaned up file control for the react project and finished project for now. Will go back later and add redux.

--- web_development/react/robofriends/src/containers/App.js
+++ web_development/react/robofriends/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -22,14 +23,19 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        return (
+        return !robots.length ? 
+        <h1>Loading</h1>:
+        (
             <div className='tc'>
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
         );
     }

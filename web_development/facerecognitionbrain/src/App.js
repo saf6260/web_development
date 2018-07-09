2c4105fdfeb46eb3bdfdc3238d1d05600931e6b0
Scott <saf6260@rit.edu>
2018-07-09 00:23:35 -0600

    Added control for sign in/register pages along with mapping for text displays. Finished front end development for the project for now. Back end development next, then database control

--- web_development/facerecognitionbrain/src/App.js
+++ web_development/facerecognitionbrain/src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Clarifai from 'clarifai';
 import './App.css';
 import Particles from 'react-particles-js';
@@ -31,6 +33,8 @@ class App extends Component {
           input: '',
           imageUrl: '',
           box: {},
+          route: 'signin',
+          isSignedIn: false,
       }
   }
 
@@ -51,9 +55,9 @@ class App extends Component {
         this.setState({box: box})
     }
 
-  onInputChange = (event) => {
-      this.setState({input: event.target.value});
-  }
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    }
 
     onButtonSubmit = () => {
       this.setState({imageUrl: this.state.input});
@@ -62,21 +66,41 @@ class App extends Component {
               Clarifai.FACE_DETECT_MODEL, 
               this.state.input)
           .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-          .catch(err => console.log(err));}
+            .catch(err => console.log(err));}
+    
+    onRouteChange = (route) => {
+        if (route === 'signout') {
+            this.setState({isSignedIn: false})
+        } else if (route === 'home') {
+            this.setState({isSignedIn: true})
+        }
+        this.setState({route: route});
+    }
 
-  render() {
+    render() {
+    const { isSignedIn, imageUrl, route, box} = this.state;
     return (
         <div className="App">
             <Particles className='particles'
               params={particlesOptions}
             />
-            <Navigation />
-            <Logo />
-            <Rank />
-            <ImageLinkForm 
-                onInputChange={this.onInputChange} 
-                onButtonSubmit={this.onButtonSubmit}/>
-            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+            <Navigation isSignedIn ={isSignedIn} onRouteChange={this.onRouteChange}/>
+            { route === 'home' ?
+                <div> 
+                    <Logo />
+                    <Rank />
+                    <ImageLinkForm 
+                        onInputChange={this.onInputChange} 
+                        onButtonSubmit={this.onButtonSubmit}
+                    />
+                    <FaceRecognition box={box} imageUrl={imageUrl}/>
+                </div>
+                : (
+                    route === 'signin' ?
+                    <Signin onRouteChange={this.onRouteChange}/>
+                    : <Register onRouteChange={this.onRouteChange}/>
+                )
+            }
         </div>
     );
   }

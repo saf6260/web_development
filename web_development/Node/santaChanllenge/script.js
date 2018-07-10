370bcb99dedcf5b439cd923f2b6a6de922485a55
Scott <saf6260@rit.edu>
2018-07-09 19:22:35 -0600

    Finished santa challenge day 1 from 2015. Compared my solution to the solution suggested and it is more optimized

--- web_development/Node/santaChanllenge/script.js
+++ web_development/Node/santaChanllenge/script.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('./input.txt', (err, data) => {
+/*fs.readFile('./input.txt', (err, data) => {
     console.time('funchallenge');
     if(err){
         console.log(err);
@@ -24,4 +24,48 @@ fs.readFile('./input.txt', (err, data) => {
         console.log(basement);
     }
     console.timeEnd('funchallenge');
-})
+})*/ //My way ^
+//The better way v
+
+function question1() {
+    fs.readFile('./input.txt', (err, data) => {
+        console.time('santa-time');
+        const directions = data.toString();
+        const directionArray = directions.split('');
+        const answer = directionArray.reduce((acc, currentValue) => {
+            if (currentValue ==="(") {
+                return acc += 1;
+            } else if (currentValue ===")"){
+                return acc -= 1;
+            }
+        },0)
+        console.timeEnd('santa-time');
+        console.log('floor:', answer);
+    })
+}
+
+question1()
+
+function question2() {
+    fs.readFile('./input.txt', (err, data) => {
+        console.time('santa-time');
+        const directions = data.toString();
+        const directionArray = directions.split('');
+        let acc = 0;
+        let counter = 0;
+        const answer = directionArray.some((currentValue) =>{
+            if(currentValue === '('){
+                acc += 1;
+            } else if(currentValue === ')'){
+                acc -= 1;
+            }
+            counter ++;
+            return acc < 0;
+        })
+        console.timeEnd('santa-time');
+        console.log('basement entered at: ', counter);
+    })
+}
+
+question2();
+

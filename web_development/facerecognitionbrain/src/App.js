e0ac13d937feb4336f9e5ac6b06699fba190391c
Scott <saf6260@rit.edu>
2018-07-11 11:53:14 -0600

    Added control for registering users, updated return information for the browser to not return the user's password once logged in, and updated git ignore to ensure node_modules are not being tracked/uploaded to github

--- web_development/facerecognitionbrain/src/App.js
+++ web_development/facerecognitionbrain/src/App.js
@@ -27,16 +27,34 @@ const particlesOptions = {
 }
 
 class App extends Component {
-  constructor() {
-      super();
-      this.state = {
-          input: '',
-          imageUrl: '',
-          box: {},
-          route: 'signin',
-          isSignedIn: false,
-      }
-  }
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+            route: 'signin',
+            isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                entries: 0,
+                joined: ''
+            }
+        }
+    }
+
+    loadUser = (data) => {
+        this.setState({user: {
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            entries: data.entries,
+            joined: data.joined,
+        }
+        })
+    }
 
     calculateFaceLocation = (data) => {
         const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -88,7 +106,7 @@ class App extends Component {
             { route === 'home' ?
                 <div> 
                     <Logo />
-                    <Rank />
+                    <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                     <ImageLinkForm 
                         onInputChange={this.onInputChange} 
                         onButtonSubmit={this.onButtonSubmit}
@@ -97,8 +115,8 @@ class App extends Component {
                 </div>
                 : (
                     route === 'signin' ?
-                    <Signin onRouteChange={this.onRouteChange}/>
-                    : <Register onRouteChange={this.onRouteChange}/>
+                    <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                    : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
                 )
             }
         </div>

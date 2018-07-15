166cd7f6cd1e8bef73eaec7c2e10890961dabd9e
Scott <saf6260@rit.edu>
2018-07-15 17:05:31 -0600

    Added extra security control so specified keys are kept more private

--- web_development/facerecognitionbrain/src/App.js
+++ web_development/facerecognitionbrain/src/App.js
@@ -6,13 +6,8 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
-import Clarifai from 'clarifai';
 import './App.css';
 import Particles from 'react-particles-js';
-
-const app = new Clarifai.App({
-    apiKey: 'd0d536197ad04ee88926cb802150f146'
-});
 
 const particlesOptions = {
     particles: {
@@ -80,11 +75,15 @@ class App extends Component {
     }
 
     onButtonSubmit = () => {
-      this.setState({imageUrl: this.state.input});
-      app.models
-            .predict(
-              Clarifai.FACE_DETECT_MODEL, 
-              this.state.input)
+        this.setState({imageUrl: this.state.input});
+        fetch('http://localhost:3000/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                input: this.state.input
+            })
+        })
+            .then(response => response.json())
             .then(response => {
                 if(response) {
                     fetch('http://localhost:3000/image', {
--- web_development/smart-brain-api/controllers/image.js
+++ web_development/smart-brain-api/controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'd0d536197ad04ee88926cb802150f146'
+});
+
+const handleApiCall = (req,res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch( err => res.status(400).json('unable to work with AI'));
+}
+
 const handleImage = (db) => (req, res) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -8,5 +23,6 @@ const handleImage = (db) => (req, res) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }

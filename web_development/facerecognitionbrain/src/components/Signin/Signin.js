939e6ffe9e425745ebc2c53f858b1a6ddddf556f
Scott <saf6260@rit.edu>
2018-07-16 13:41:34 -0600

    Added routing for heroku hosting of backend. Note API key is now non-functional to preserve security measures

--- web_development/facerecognitionbrain/src/components/Signin/Signin.js
+++ web_development/facerecognitionbrain/src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://lit-reaches-56616.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({

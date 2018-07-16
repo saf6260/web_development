7178bcd3b2b23e2489c596e2e621c97da2bad4c2
Scott <saf6260@rit.edu>
2018-07-16 00:08:04 -0600

    Adding files to new github repository for heroku upload

--- web_development/smart-brain-api/server.js
+++ web_development/smart-brain-api/server.js
@@ -2,93 +2,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'test',
+        database: 'smart-brain'
+    }
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date(),
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date(),
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password){
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
-})
+app.post('/signin', signin.handleSignin(db, bcrypt))
 
-app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
-})
+app.post('/register', register.handleRegister(db, bcrypt))
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    })
-    if(!found){
-        res.status(400).json('not found');
-    }
-})
+app.get('/profile/:id', profile.handleProfileGet(db))
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;   
-            user.entries ++;
-            return res.json(user.entries);
-        }
-    })
-    if(!found){
-        res.status(400).json('not found');
-    }
-})
+app.put('/image', image.handleImage(db))
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 })
